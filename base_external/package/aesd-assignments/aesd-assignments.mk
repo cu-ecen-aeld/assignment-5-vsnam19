c3914de987329d8ff004d8b7dcf798e75404d7bc
namvs <vsnam19@outlook.com>
2025-12-29 00:31:57 +0700

    Correct githash

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '4b6a1f70bc2596855f704574cc59a834f2977b42'
+AESD_ASSIGNMENTS_VERSION = '8114bf96949b21861f450a6df25ab492ff05c8c2'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
